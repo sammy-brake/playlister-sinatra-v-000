2d7dc15396e50da5d9bf65b9791cfad5f13e78bc
Samantha brake <sammy.brake@gmail.com>
2019-03-17 19:24:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,8 +14,8 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(name: params[:Name])
-binding.pry
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+
+    @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
     # binding.pry
     @song.genres << Genre.find_by_id(params[:genres])
     @song.save
