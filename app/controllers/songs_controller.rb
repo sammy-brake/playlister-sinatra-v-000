4fa8315104d235f1fe1b6cb4fddae4da12335b60
Samantha brake <sammy.brake@gmail.com>
2019-03-17 20:29:18 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,7 +50,7 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     binding.pry
-    @song.update(params[:song])
+    @song.update(name: params[:Name])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
     flash[:message] = "Successfully updated song."
