8ec5c283612eaa56af817021b7f1c73cc939a271
Samantha brake <sammy.brake@gmail.com>
2019-03-17 19:30:26 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
   # end
 
   get '/songs/:slug' do
-    binding.pry
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
